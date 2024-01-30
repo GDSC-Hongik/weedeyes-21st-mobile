d88b15d1e7139849b516463fa9e9dd91f2063d67
hhhkdev <c235335@g.hongik.ac.kr>
2024-01-31 07:57:38 +0900

    feat: 버튼 형태 구현

--- weedeyes/lib/widgets/button_medium.dart
+++ weedeyes/lib/widgets/button_medium.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ButtonMedium extends StatelessWidget {
   final String? text;
@@ -6,6 +6,19 @@ class ButtonMedium extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoButton(child: Text("$text"), onPressed: () {});
+    return ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          fixedSize: const Size(300, 56),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+          backgroundColor: Color.fromRGBO(52, 220, 118, 1),
+        ),
+        child: Text(
+          "$text",
+          style: const TextStyle(
+              color: Colors.white, fontSize: 18, fontWeight: FontWeight.normal),
+        ));
   }
 }
--- weedeyes/lib/widgets/button_small.dart
+++ weedeyes/lib/widgets/button_small.dart
@@ -1,11 +1,24 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ButtonSmall extends StatelessWidget {
   final String? text;
-  const ButtonSmall({Key? key, this.text});
+  const ButtonSmall({super.key, required this.text});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoButton(child: Text("$text"), onPressed: () {});
+    return ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          fixedSize: const Size(144, 56),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+          backgroundColor: Color.fromRGBO(52, 220, 118, 1),
+        ),
+        child: Text(
+          "$text",
+          style: const TextStyle(
+              color: Colors.white, fontSize: 18, fontWeight: FontWeight.normal),
+        ));
   }
 }
