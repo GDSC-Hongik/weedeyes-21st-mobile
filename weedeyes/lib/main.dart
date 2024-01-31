e871efa071e39c0d21ab4cfd4882ec2c45cefb3a
hhhkdev <c235335@g.hongik.ac.kr>
2024-01-31 09:17:41 +0900

    feat: Input 위젯 형태 구현

--- weedeyes/lib/main.dart
+++ weedeyes/lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:weedeyes/widgets/button_medium.dart';
 import 'package:weedeyes/widgets/button_small.dart';
 import 'package:weedeyes/widgets/option.dart';
+import 'package:weedeyes/widgets/search.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -20,14 +21,15 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(),
-        body: const Column(children: [
+        body: Column(children: [
           ButtonSmall(
             text: "확인",
           ),
           ButtonMedium(
             text: "검색",
           ),
-          Option(text: "대마 사진이 발견되었습니다.", option1: "확인", option2: "재촬영")
+          Option(text: "대마 사진이 발견되었습니다.", option1: "확인", option2: "재촬영"),
+          Search(),
         ]),
       ),
     );
