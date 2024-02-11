c79d27b954b91cd54b15a99ac244b706f3e3e7dd
hhhkdev <c235335@g.hongik.ac.kr>
2024-02-12 00:25:47 +0900

    fix: 오타 수정

--- weedeyes/lib/main.dart
+++ weedeyes/lib/main.dart
@@ -19,6 +19,6 @@ class Main extends StatelessWidget {
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
             fontFamily: "Pretendard"),
-        home: AuthLocation());
+        home: Initial());
   }
 }
